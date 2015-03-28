ef55abe933031f98fcb6242793202ce9c009d007
katecaban <kate_caban@icloud.com>
2015-03-27 21:46:11 -0400

    Revert "color-prompt"
    This reverts commit a2ccd36e5bcf61f717dc1825117ed74067d55ed9.

--- week4homework/js/project.js
+++
@@ -1,9 +0,0 @@
-function changeColor(){
-  var a = document.getElementById("red").value;
-  var b = document.getElementById("green").value;
-  var c = document.getElementById("blue").value;
-  document.body.style.backgroundColor = 'rgb(' + a + ',' + b + ',' + c + ')';
-  document.getElementById("colorfultext").innerHTML = 'rgb(' + a + ',' + b + ',' + c + ')';
-}
-document.getElementById("colorbutton").onclick = changeColor;
-
