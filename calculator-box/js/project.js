cd5752aa8f64506d9b14e1ec4828900d1760ce5a
katecaban <kate_caban@icloud.com>
2015-04-04 20:37:30 -0400

    calculator-box1

--- calculator-box/js/project.js
+++ calculator-box/js/project.js
@@ -4,18 +4,21 @@ document.getElementById("a10").onclick = a10;
 function a10() {
   total = total + 10;
   document.getElementById("out").innerHTML = total;
+  document.getElementById("out").style.backgroundColor = "white";
 }
 
 document.getElementById("a20").onclick = a20;
 function a20() {
   total = total + 20;
   document.getElementById("out").innerHTML = total;
+  document.getElementById("out").style.backgroundColor = "white";
 }
 
 document.getElementById("a30").onclick = a30;
 function a30() {
   total = total + 30;
   document.getElementById("out").innerHTML = total;
+  document.getElementById("out").style.backgroundColor = "white";
 }
 
 document.getElementById("red").onclick = red;
@@ -39,16 +42,20 @@ document.getElementById("n10").onclick = n10;
 function n10() {
   total = total - 10;
   document.getElementById("out").innerHTML = total;
+  document.getElementById("out").style.backgroundColor = "white";
 }
 
 document.getElementById("n20").onclick = n20;
 function n20() {
   total = total - 20;
   document.getElementById("out").innerHTML = total;
+  document.getElementById("out").style.backgroundColor = "white";
 }
 
 document.getElementById("n30").onclick = n30;
 function n30() {
   total = total - 30;
   document.getElementById("out").innerHTML = total;
+  document.getElementById("out").style.backgroundColor = "white";
 }
+
